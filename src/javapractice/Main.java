d54ae3981f5f0a33b515f094047c2f27bfd4b5c0
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-01-03 23:13:43 +0600

    Refactor Main class to include variable declarations and outputs for demonstration

--- src/javapractice/Main.java
+++ src/javapractice/Main.java
@@ -3,6 +3,23 @@ package javapractice;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello,World!");
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+
+        char myGrade = 'A';
+        System.out.println(myGrade);
+
+        String myName = "John";
+        System.out.println(myName);
+
+        short myShort = 5000;
+        System.out.println(myShort);
+
+        long myLong = 150000L;
+        System.out.println(myLong);
+        
+        double myDouble = 19.99d;
+        System.out.println(myDouble);
+        System.out.println("Hello, World!");
     }
 }
